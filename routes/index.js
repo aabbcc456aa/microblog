813d6f67903afca83361616a1e6b6323803777f6
root <root@huxl>
2014-07-22 17:48:11 +0800

    first commit

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  res.render('index', { title: 'Express' });
+};
+
+exports.user = function(reg,res){
+};
+
+exports.post = function(reg,res){
+};
+
+exports.reg = function(reg,res){
+};
+
+exports.doReg = function(reg,res){
+};
+
+exports.login = function(reg,res){
+};
+
+exports.doLogin = function(reg,res){
+};
+
+exports.logout = function(reg,res){
+};
+
---
+++ settings.js
@@ -0,0 +1,8 @@
+/**
+ * Created by root on 14-7-22.
+ */
+module.exports = {
+    cookieSecret: 'microblogbyvoid',
+    db: 'microblog',
+    host: 'localhost'
+};
