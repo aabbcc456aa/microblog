4d4cb89dac857b6dbb8468854fbb4ac779ec42c3
root <root@huxl>
2014-07-23 11:01:03 +0800

    extract  setting of route to file /routes/index.ejs add new npm package 'express-ejs-layouts'  ,beause express 3.x ejs not suppor layout

--- app.js
+++ app.js
@@ -3,50 +3,60 @@
  * Module dependencies.
  */
 
-
 var express = require('express');
 var settings = require('./settings');
 var MongoStore = require('connect-mongo')(express);
 var routes = require('./routes');
-//var user = require('./routes/user');
+var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var expressLayouts = require('express-ejs-layouts');
+
 
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 4000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(express.bodyParser());
-app.use(express.cookieParser());
-app.use(express.session({
-    secret: settings.cookieSecret,
-    store: new MongoStore({
-        db: settings.db
-    })
-}));
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.configure(function() {
+    app.set('port', process.env.PORT || 4000);
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'ejs');
+    app.set('layout', 'layout');
+    app.use(expressLayouts);
+    app.use(express.bodyParser());
+    app.use(express.cookieParser());
+    app.use(express.session({
+        secret: settings.cookieSecret,
+        store: new MongoStore({
+            db: settings.db
+        })
+    }));
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.json());
+    app.use(express.urlencoded());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    //app.use(express.router(routes));
+    app.use(express.static(path.join(__dirname, 'public')));
+});
+
+routes(app);
+
+
 
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/u/:user', routes.user);
-app.post('/post', routes.post);
-app.get('/reg', routes.reg);
-app.post('/post',routes.doReg);
-app.get('/login',routes.login);
-app.post('/login', routes.doLogin);
-app.get('/logout',routes.logout);
+//app.get('/', routes.index);
+//app.get('/u/:user', routes.user);
+//app.post('/post', routes.post);
+//app.get('/reg', routes.reg);
+//app.post('/post',routes.doReg);
+//app.get('/login',routes.login);
+//app.post('/login', routes.doLogin);
+//app.get('/logout',routes.logout);
 //app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,37 @@
-
 /*
  * GET home page.
  */
+//
+//exports.index = function(req, res){
+//  res.render('index', { title: 'Express' });
+//};
+//
+//exports.user = function(reg,res){
+//};
+//
+//exports.post = function(reg,res){
+//};
+//
+//exports.reg = function(reg,res){
+//};
+//
+//exports.doReg = function(reg,res){
+//};
+//
+//exports.login = function(reg,res){
+//};
+//
+//exports.doLogin = function(reg,res){
+//};
+//
+//exports.logout = function(reg,res){
+//};
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
-};
-
-exports.user = function(reg,res){
-};
-
-exports.post = function(reg,res){
-};
-
-exports.reg = function(reg,res){
-};
-
-exports.doReg = function(reg,res){
-};
-
-exports.login = function(reg,res){
-};
-
-exports.doLogin = function(reg,res){
-};
-
-exports.logout = function(reg,res){
-};
-
+module.exports = function (app) {
+    app.get('/', function (req, res) {
+        res.render('index', { title: "首页"   , layout: 'layout'});
+    });
+    app.get("/reg", function (req, res) {
+        res.render('reg', { title: "用户注册" });
+    });
+}
